173310399d9bcd6b3466b78855e0ee8e9a18ba1a
Carlos <cnoramb1@my.centennialcollege.ca>
2023-03-04 13:27:42 -0500

    Blood Oxygen Level/Add BO Record & Edit BO Record & BO Record List Screen Done

--- lib/Medical Records/Blood Oxygen Level/bloodoxygenlevel.dart
+++ lib/Medical Records/Blood Oxygen Level/bloodoxygenlevel.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:patient_data_mobileapp/Medical%20Records/Blood%20Oxygen%20Level/bloodoxygenlevel.dart';
-import 'package:patient_data_mobileapp/Medical%20Records/Blood%20Pressure/bloodpressure.dart';
-import 'package:patient_data_mobileapp/Medical%20Records/Respiratory%20Rate/respiratoryrate.dart';
+import 'package:patient_data_mobileapp/Medical%20Records/Blood%20Oxygen%20Level/addborecord.dart';
+import 'package:patient_data_mobileapp/Medical%20Records/Blood%20Oxygen%20Level/bolist.dart';
+import 'package:patient_data_mobileapp/Medical%20Records/Blood%20Oxygen%20Level/editborecord.dart';
 
 // ignore: camel_case_types
-class Medical_Records extends StatelessWidget {
-  const Medical_Records({super.key});
+class Blood_Oxygen_Level extends StatelessWidget {
+  const Blood_Oxygen_Level({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Medical Records"),
+        title: const Text("Blood Oxygen Level"),
       ),
       body: Center(
         child: Column(
@@ -30,13 +30,11 @@ class Medical_Records extends StatelessWidget {
                 shape: const BeveledRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10))),
               ),
-              child: const Text("Blood Pressure"),
+              child: const Text("Add BO Record"),
               onPressed: () {
-                // Navigation to Blood Pressure.
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const Blood_Pressure()));
+                // Navigation to Add BO Record.
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Add_BO_Record()));
               },
             ),
 //
@@ -53,13 +51,13 @@ class Medical_Records extends StatelessWidget {
                 shape: const BeveledRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10))),
               ),
-              child: const Text("Respiratory Rate"),
+              child: const Text("Edit BO Record"),
               onPressed: () {
-                // Navigation to Respiratory Rate.
+                // Navigation to Edit BO Record.
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const Respiratory_Rate()));
+                        builder: (context) => const Edit_BO_Record()));
               },
             ),
 //
@@ -76,36 +74,13 @@ class Medical_Records extends StatelessWidget {
                 shape: const BeveledRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10))),
               ),
-              child: const Text("Blood Oxygen Level"),
+              child: const Text("BO Lsit"),
               onPressed: () {
-                // Navigation to Blood Oxygen Level.
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const Blood_Oxygen_Level()));
+                // Navigation to BO Lsit.
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const BO_List()));
               },
             ),
-//
-            const SizedBox(
-              height: 20,
-            ),
-//
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                textStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                    fontStyle: FontStyle.normal),
-                shape: const BeveledRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-              ),
-              child: const Text("Heart Beat Rate"),
-              onPressed: () {
-                // Navigation to Heart Beat Rate.
-                //   Navigator.push(context,
-                //       MaterialPageRoute(builder: (context) => const Heart_Beat_Rate()));
-              },
-            )
           ],
         ),
       ),
